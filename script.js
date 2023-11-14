d13c0dfcceaf422b0b46a04423271263627411c5
Hachi <h4ch1net@gmail.com>
2023-11-14 15:36:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,11 @@ const h4ch1netElement = document.getElementById('h4ch1net');
 h4ch1netElement.addEventListener('click', () => {
     console.log('Clicked!'); // Log to the console for debugging
 
-    // Add the "shake" class to trigger the shake animation
-    h4ch1netElement.classList.add('shake');
+    // Remove all classes to stop any ongoing animations
+    h4ch1netElement.classList.remove('shake');
 
-    // Remove the "shake" class after the shake animation completes
-    setTimeout(() => {
-        h4ch1netElement.classList.remove('shake');
-    }, 500); // 500ms matches the shake animation duration
+    // Reset the glitch animation
+    h4ch1netElement.style.animation = '';
 
-    // Trigger the glitch effect
-    h4ch1netElement.style.animation = 'glitch 3s infinite';
-    setTimeout(() => {
-        h4ch1netElement.style.animation = ''; // Reset the glitch animation
-    }, 3000); // 3000ms matches the glitch animation duration
+    // Add any additional actions you want to perform on click
 });
