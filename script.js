9283be58b672d06c2c9b9207ca7fd730a3fa5c8b
Hachi <h4ch1net@gmail.com>
2023-11-14 18:43:32 -0800

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
     const h1Element = document.getElementById("h4ch1net");
 
@@ -10,6 +8,35 @@ document.addEventListener("DOMContentLoaded", function () {
         // Remove the class after another 2 seconds to revert to the original state
         setTimeout(() => {
             h1Element.classList.remove("eyes-transform");
-        }, 20000);
+        }, 2000);
     }, 10000);
+
+    const titleSequence = [
+        { title: "H4ch1net", delay: 2000 },
+        { title: "👁️👁️", delay: 500 },
+        { title: "👁️", delay: 5 },
+        { title: "H4ch1net", delay: 6000 },
+        { title: "Nerd", delay: 100 }
+    ];
+
+    function changeTitle(newTitle, delay) {
+        setTimeout(function () {
+            document.title = newTitle;
+        }, delay);
+    }
+
+    function titleLoop() {
+        let currentIndex = 0;
+
+        function nextTitle() {
+            changeTitle(titleSequence[currentIndex].title, titleSequence[currentIndex].delay);
+            currentIndex = (currentIndex + 1) % titleSequence.length;
+            setTimeout(nextTitle, titleSequence[currentIndex].delay);
+        }
+
+        nextTitle();
+    }
+
+    // Start the title loop
+    titleLoop();
 });
